5ac5d2e603c429f97ff9752a4c298c468061f293
Patrick <patrick@peakinformatik.com>
2014-06-28 23:47:54 +0200

    Added cookie builder

---
+++ src/CookieBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Http;
+
+class CookieBuilder
+{
+    private $defaultDomain;
+    private $defaultPath = '/';
+    private $defaultSecure = true;
+    private $defaultHttpOnly = true;
+
+    public function setDefaultDomain($domain)
+    {
+        $this->defaultDomain = (string) $domain;
+    }
+
+    public function setDefaultPath($path)
+    {
+        $this->defaultPath = (string) $path;
+    }
+
+    public function setDefaultSecure($secure)
+    {
+        $this->defaultSecure = (bool) $secure;
+    }
+
+    public function setDefaultHttpOnly($httpOnly)
+    {
+        $this->defaultHttpOnly = (bool) $httpOnly;
+    }
+
+    public function build($name, $value)
+    {
+        $cookie = new Cookie($name, $value);
+        $cookie->setPath($this->defaultPath);
+        $cookie->setSecure($this->defaultSecure);
+        $cookie->setHttpOnly($this->defaultHttpOnly);
+
+        if ($this->defaultDomain !== null) {
+            $cookie->setDomain($this->defaultDomain);
+        }
+
+        return $cookie;
+    }
+}
