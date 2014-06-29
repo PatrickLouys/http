a5e01b38cba7564347b1d1e210c3486b4ba802a0
Patrick Louys <i@hehejo.info>
2014-06-29 14:17:56 +0200

    Update MissingRequestMetaVariableException.php

--- src/MissingRequestMetaVariableException.php
+++ src/MissingRequestMetaVariableException.php
@@ -4,7 +4,7 @@ namespace Http;
 
 class MissingRequestMetaVariableException extends \Exception
 {
-    public function __construct($variableName, $code = 0, Exception $previous = null) {
+    public function __construct($variableName, $code = 0, \Exception $previous = null) {
         $message = "Request meta-variable $variableName was not set.";
 
         parent::__construct($message, $code, $previous);
