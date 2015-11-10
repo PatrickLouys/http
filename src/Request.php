23ae663938592baa23e60711850a675d36454961
Hassan Althaf <HassanAlthaf@users.noreply.github.com>
2015-11-10 15:48:29 +0530

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -7,7 +7,6 @@ interface Request
     public function getParameter($key, $defaultValue = null);
     public function getQueryParameter($key, $defaultValue = null);
     public function getBodyParameter($key, $defaultValue = null);
-    public function getParameter($key, $defaultValue = null);
     public function getFile($key, $defaultValue = null);
     public function getCookie($key, $defaultValue = null);
     public function getParameters();
