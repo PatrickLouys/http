3cbdb0cf57130710fab7046d9eb78641839d16b1
Hassan Althaf <HassanAlthaf@users.noreply.github.com>
2015-11-10 15:42:22 +0530

    Updated a bit for the new issue.

--- src/Request.php
+++ src/Request.php
@@ -5,9 +5,14 @@ namespace Http;
 interface Request
 {
     public function getParameter($key, $defaultValue = null);
+    public function getQueryParameter($key, $defaultValue = null);
+    public function getBodyParameter($key, $defaultValue = null);
+    public function getParameter($key, $defaultValue = null);
     public function getFile($key, $defaultValue = null);
     public function getCookie($key, $defaultValue = null);
     public function getParameters();
+    public function getQueryParameters();
+    public function getBodyParameters();
     public function getCookies();
     public function getFiles();
     public function getUri();
